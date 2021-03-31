668785535645b7e34e9480703ad074184f54d14d
Allan Anderson <allana@shaw.ca>
2021-03-31 07:38:30 -0600

    Update matrix4.h

--- include/matrix4.h
+++ include/matrix4.h
@@ -1,5 +1,7 @@
 //This class represents a 4x4 matrix, i.e.,
-//  [ M11  M12  M13  M14 ]
-//  [ M21  M22  M23  M24 ]
-//  [ M31  M32  M33  M34 ]
-//  [ M41  M42  M43  M44 ]
+//     ┌                          ┐
+//     | M11    M12    M13    M14 |
+//     | M21    M22    M23    M24 |
+//     | M31    M32    M33    M34 |
+//     | M41    M42    M43    M44 |
+//     └                          ┘
