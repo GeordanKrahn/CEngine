62fb03cadc14adf024dbc50e3a6f4004d0620931
Allan Anderson <allana@shaw.ca>
2021-03-31 12:08:16 -0600

    Update functions.h

--- include/functions.h
+++ include/functions.h
@@ -1,1 +1,10 @@
 //This is a generic class to hold miscellaneous math functions required for the engine
+
+//Angle conversions degrees <--> radians
+double toRadians(double degrees);
+double toDegrees(double radians);
+
+//Quadratic equation solvers
+double quadraticNegative(double a, double b, double c);
+double quadraticPositive(double a, double b, double c);
+
