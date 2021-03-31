b0b0463f7ce1c5ff547854e8f64e8222c7b03a92
Allan Anderson <allana@shaw.ca>
2021-03-31 07:40:15 -0600

    Create matrix3.cpp

---
+++ src/matrix3.cpp
@@ -0,0 +1,6 @@
+//This class is the implementation if include/matrix3.h
+//     ┌                   ┐
+//     | M11    M12    M13 |
+//     | M21    M22    M23 |
+//     | M31    M32    M33 |
+//     └                   ┘
