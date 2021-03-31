74407b29c73276e58c9701541dbadac1a2dd8076
Allan Anderson <allana@shaw.ca>
2021-03-31 12:21:55 -0600

    Update functions.cpp

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,1 +1,28 @@
 //This class file is the implementation of include/functions.h
+
+#include <cmath>
+#include "../include/functions.h"
+
+//Angle conversions degrees <--> radians
+double toRadians(double degrees)
+{
+  return degrees * M_PI / 180; 
+}
+
+double toDegrees(double radians)
+{
+  return radians * 180 / M_PI;
+}
+
+//Quadratic solvers
+double quadraticNegative(double a, double b, double c)
+{
+  double b2 = b * b, ac4 = 4 * a * c, a2 = 2 * a, root = b2 - ac4;
+  return (-1 * b - sqrt(root)) / a2;
+}
+
+double quadraticPositive(double a, double b, double c)
+{
+   double b2 = b * b, ac4 = 4 * a * c, a2 = 2 * a, root = b2 - ac4;
+  return (-1 * b + sqrt(root)) / a2;
+}
