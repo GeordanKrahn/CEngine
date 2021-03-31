cbfae475514352a83bb546843ccbfedf4a7a52a0
Allan Anderson <allana@shaw.ca>
2021-03-31 07:37:53 -0600

    Update matrix3.h

--- include/matrix3.h
+++ include/matrix3.h
@@ -1,4 +1,6 @@
 //This class represents a 3x3 matrix, i.e.,
-//  [M11  M12  M13 ]
-//  [M21  M22  M23 ]
-//  [M31  M32  M33 ]
+//     ┌                   ┐
+//     | M11    M12    M13 |
+//     | M21    M22    M23 |
+//     | M31    M32    M33 |
+//     └                   ┘
