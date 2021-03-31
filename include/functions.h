e512cffa1b3105f02ccf1c6c8e79df882470c5f0
Allan Anderson <allana@shaw.ca>
2021-03-31 12:32:30 -0600

    Update functions.h

--- include/functions.h
+++ include/functions.h
@@ -1,4 +1,5 @@
 //This is a generic class to hold miscellaneous math functions required for the engine
+#include <tuple>
 
 //Angle conversions degrees <--> radians
 double toRadians(double degrees);
@@ -8,3 +9,10 @@ double toDegrees(double radians);
 double quadraticNegative(double a, double b, double c);
 double quadraticPositive(double a, double b, double c);
 
+//Trigonometry solvers
+std::tuple<double, double> calculateAdjacentOpposite(double degrees, double hypotenuse);
+std::tuple<double, double> calculateAdjacentHypotenuse(double degrees, double opposite);
+std::tuple<double, double> calculateOppositeHypotenuse(double degrees, double adjacent);
+std::tuple<double, double> calculateHypotenuseTheta(double adjacent, double opposite);
+std::tuple<double, double> calculateAdjacentTheta(double opposite, double hypotenuse);
+std::tuple<double, double> calculateOppositeTheta(double adjacent, double hypotenuse);
