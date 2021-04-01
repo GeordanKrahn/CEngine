34058d00f9a64c983d07445b8c295175a0ff6efc
Allan Anderson <allana@shaw.ca>
2021-03-31 19:25:34 -0600

    Update point2.h

--- include/point2.h
+++ include/point2.h
@@ -1,1 +1,25 @@
 //This class represents a Point in 2D space
+
+#ifndef POINT2_H
+#define POINT2_H
+
+class Point2
+{
+  private:
+    double m_X;
+    double m_Y;
+  
+  public:
+    double getX() { return m_X; }
+    void setX(double x) { m_X = x; }
+  
+    void setY(double y) { m_Y = y; }
+    double getY() { return m_Y; }
+  
+    Point2();
+    Point2(double x, double y);
+  
+    double segmentLength(Point2 b);
+    Point2 midPoint(Point2 b);
+};
+#endif
