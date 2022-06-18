2de0c2f9c14e73f51b51b5e01f6de38f39d0e28f
Geordan Krahn <geordankrahn@gmail.com>
2022-06-18 16:41:39 -0600

    Point2D: class definition, member functions Point2D: non member overloads

--- include/point2.h
+++ include/point2.h
@@ -1,1 +1,32 @@
 //This is a Point in 2D space
+namespace Engine 
+{
+    namespace Mathematics
+    {
+        class Point2D
+        {
+        private:
+        // properties
+            double _X;
+            double _Y;
+        public:
+        // Setter & Getters
+            const double & X = _X;
+            const double & Y = _Y;
+            void SetX(double X);
+            void SetY(double Y);
+        
+        // Constructors
+            Point2D() : _X(0), _Y(0) {}
+            Point2D(double X, double Y) : _X(X), _Y(Y) {}
+        // copy constructor and assignment overload.
+            Point2D & operator = (const Point2D & rh){ return Point2D(rh.X, rh.Y); }
+        // class methods
+            double SegmentLength(const Point2D & b) const;
+            Point2D * MidPoint(const Point2D & b) const;
+
+        // Operator Overloads
+            // non member functions
+        };
+    }
+}
--- src/point2.cpp
+++ src/point2.cpp
@@ -1,1 +1,35 @@
 //This class is the implementation of include.point2.h
+#pragma once
+#include "point2.h"
+#include <math.h>
+namespace Engine
+{
+    namespace Mathematics
+    {
+        // setters
+        void Point2D::SetX(double X) { _X = X; }
+        void Point2D::SetY(double Y) { _Y = Y; }
+        // class methods
+        double Point2D::SegmentLength(const Point2D & b) const
+        {
+            return sqrt( pow(b.X - X, 2) + pow(b.Y - Y, 2));
+        }
+        Point2D * Point2D::MidPoint(const Point2D & b) const
+        {
+            return new Point2D(0.5 * (b.X + X), 0.5 * (b.Y + Y));
+        }
+        // non member operator overloads
+        Point2D operator *(const double & lh, const Point2D & rh)
+        {
+            return Point2D(lh * rh.X, lh * rh.Y);
+        }
+        bool operator ==(const Point2D & a, const Point2D & b)
+        {
+            return (a.X == b.X && a.Y == b.Y);
+        }
+        bool operator !=(const Point2D & a, const Point2D & b)
+        {
+            return !(a==b);
+        }
+    }
+}
