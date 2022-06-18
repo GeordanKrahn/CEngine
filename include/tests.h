6fc34aebe4868f06d8e50ad1873f899f28f47528
Geordan Krahn <geordankrahn@gmail.com>
2022-06-18 17:02:08 -0600

    Tests: Set up layout for testing

---
+++ include/tests.h
@@ -0,0 +1,88 @@
+#pragma once
+
+// Test Point2D
+void RunTestPoint2D()
+{
+
+
+    // Test Empty Constructor
+
+    // Test Greedy Constructor
+
+    // Test Copy Constructor
+    
+    // Test Setters
+
+    // Test Segment Length
+
+    // Test Midpoint
+
+    // Test Equality
+
+    // Test Inequality
+
+    // Test Scalar Multiplication
+
+}
+
+
+// Test Functions
+void RunTestFunctions()
+{
+
+}
+
+// Test Vector2D
+void RunTestVector2D()
+{
+    
+}
+
+// Test Vector3D
+void RunTestVector3D()
+{
+    
+}
+
+// Test Matrix2D
+void RunTestMatrix2D()
+{
+    
+}
+
+// Test Matrix3D
+void RunTestMatrix3D()
+{
+    
+}
+
+// Test Matrix4D
+void RunTestMatrix4D()
+{
+    
+}
+
+// Test Quaternion
+void RunTestQuaternion()
+{
+    
+}
+
+// Test Body
+void RunTestBody()
+{
+    
+}
+
+void RunAllTests()
+{
+    RunTestPoint2D();
+    RunTestFunctions();
+    RunTestVector2D();
+    RunTestVector3D();
+    RunTestMatrix2D();
+    RunTestMatrix3D();
+    RunTestMatrix4D();
+    RunTestQuaternion();
+    RunTestBody();
+}
---
+++ src/Tests.cpp
@@ -0,0 +1,11 @@
+// This is where tests will be performs on the math and physics
+#pragma once
+#include <iostream>
+#include "tests.h"
+
+using namespace std;
+
+int main()
+{
+    RunAllTests();
+}
