ff6f22cd44a0fab83633410d68a73056491e92f3
Geordan Krahn <geordankrahn@gmail.com>
2022-06-20 16:54:01 -0600

    Switched to Google Tests

---
+++ UnitTests/test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "../include/body.h"
+#include "../include/functions.h"
+#include "../include/matrix2.h"
+#include "../include/matrix3.h"
+#include "../include/matrix4.h"
+#include "../include/point2.h"
+#include "../include/quaternion.h"
+#include "../include/vector2.h"
+#include "../include/vector3.h"
+#include "gtest/gtest.h"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
