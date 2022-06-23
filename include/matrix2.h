651b483ea2f316cdb49c012e2db35f894afb641d
Geordan Krahn <geordankrahn@gmail.com>
2022-06-22 18:14:46 -0600

    Adds matrix 2x2, adds file vector4

--- include/matrix2.h
+++ include/matrix2.h
@@ -3,3 +3,36 @@
 //     | M11    M12 |
 //     | M21    M22 |
 //     └            ┘
+#pragma once
+#include <iostream>
+#include <ostream>
+#include <math.h>
+#include <memory>
+#include "../include/vector2.h"
+
+namespace Engine
+{
+	namespace Mathematics
+	{
+		class Matrix_2x2
+		{
+		private:
+			double m[2][2] {0};
+			double &_11 = m[0][0], &_12 = m[0][1], &_21 = m[1][0], &_22 = m[1][1];
+		public:
+			const double& m11 = m[0][0], m12 = m[0][1], m21 = m[1][0], m22 = m[1][1];
+			Matrix_2x2();
+			Matrix_2x2(const double& m11, const double& m12, const double& m21, const double& m22);
+			Matrix_2x2(const double arr[2][2]);
+			Matrix_2x2(const Vector2 arr[2]);
+			Matrix_2x2(const Vector2& a, const Vector2& b);
+			Matrix_2x2(const Matrix_2x2& m);
+			Matrix_2x2 Transpose() const { return Matrix_2x2(_11, _21, _12, _22); }
+			static Matrix_2x2 Transpose(const Matrix_2x2& m) { return m.Transpose(); }
+			Matrix_2x2 operator =(const Matrix_2x2& m) const;
+			void SetElement(const int& row, const int& col, const double& val);
+			double Determinant() const;
+			static double Determinant(const Matrix_2x2& m);
+		};
+	}
+}
--- src/matrix2.cpp
+++ src/matrix2.cpp
@@ -3,3 +3,70 @@
 //     | M11    M12 |
 //     | M21    M22 |
 //     └            ┘
+#pragma once
+#include <iostream>
+#include <ostream>
+#include <math.h>
+#include <memory>
+#include "../include/matrix2.h"
+namespace Engine
+{
+	namespace Mathematics
+	{
+		Matrix_2x2::Matrix_2x2()
+		{
+			_11 = 0;
+			_12 = 0;
+			_21 = 0;
+			_22 = 0;
+		}
+
+		Matrix_2x2::Matrix_2x2(const double& m11, const double& m12, const double& m21, const double& m22)
+		{
+			_11 = m11;
+			_12 = m12;
+			_21 = m21;
+			_22 = m22;
+		}
+
+		Matrix_2x2::Matrix_2x2(const double arr[2][2])
+		{
+			_11 = arr[0][0];
+			_12 = arr[0][1];
+			_21 = arr[1][0];
+			_22 = arr[1][1];
+		}
+
+		Matrix_2x2::Matrix_2x2(const Vector2 arr[2])
+		{
+			_11 = arr[0].x;
+			_12 = arr[1].x;
+			_21 = arr[0].y;
+			_22 = arr[1].y;
+		}
+
+		Matrix_2x2::Matrix_2x2(const Vector2& a, const Vector2& b)
+		{
+			_11 = a.x;
+			_12 = b.x;
+			_21 = a.y;
+			_22 = b.y;
+		}
+
+		Matrix_2x2::Matrix_2x2(const Matrix_2x2& m)
+		{
+			_11 = m.m11;
+			_12 = m.m12;
+			_21 = m.m21;
+			_22 = m.m22;
+		}
+
+		Matrix_2x2 Matrix_2x2::operator =(const Matrix_2x2& m) const{ return Matrix_2x2(m); }
+		void Matrix_2x2::SetElement(const int& row, const int& col, const double& val)
+		{
+			if(!(row > 2 || row < 1 || col > 2 || col < 1))	{ m[row - 1][col - 1] = val; }
+		}
+		double Matrix_2x2::Determinant() const { return m[0][0] * m[1][1] - m[0][1] * m[1][0]; }
+		double Matrix_2x2::Determinant(const Matrix_2x2& m) { return m.Determinant(); }
+	}
+}
